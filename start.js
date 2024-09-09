81f5fddad0f781f224fda4d53e46056735f36e56
zerolivesleft <51168812+zerolivesleft@users.noreply.github.com>
2024-09-09 14:42:38 -0500

    listen on all interfaces

--- start.js
+++ start.js
@@ -2,8 +2,8 @@ const { spawn } = require('child_process');
 const { wss, server, PORT } = require('./webserver');
 
 // Start the webserver
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 wss.on('connection', (ws) => {
