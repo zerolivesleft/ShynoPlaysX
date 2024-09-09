d6321bbc8e6d3da6fa8a18c6a9ed42b4e56c897f
zerolivesleft <51168812+zerolivesleft@users.noreply.github.com>
2024-09-09 14:27:20 -0500

    change port

--- webserver.js
+++ webserver.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
 
 const wss = new WebSocket.Server({ server });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
